126407d8e61d8e35c843a1f2ded6d4775a4889c0
Tom <rizzitom52@gmail.com>
2024-11-25 08:58:41 +0100

    Ajout du structure_android.dart

---
+++ lib/Structure_Android.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  Icon _coeur = Icon(Icons.favorite_border, color: Colors.white,);
+  bool _likeBool = false;
+
+  void _likeThis(){
+    setState(() {
+      if (_likeBool){
+        _coeur = Icon(Icons.favorite_border, color: Colors.white,);
+        bool _likeBool = false;
+      }
+      else {
+        _coeur = Icon(Icons.favorite, color: Colors.white,);
+        bool _likeBool = true;
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(widget.title),
+          actions: <Widget>[
+            IconButton(onPressed: _likeThis, icon: _coeur)
+        ],
+        backgroundColor: Colors.red,
+        ),
+    );
+  }
+}
