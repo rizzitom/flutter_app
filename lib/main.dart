c1788a5d7ce80cc13932afd2330593b498795a7c
Tom <rizzitom52@gmail.com>
2024-12-02 10:19:06 +0100

    pavlova aux fraises brut

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,6 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -32,44 +31,55 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: AppBar(
-
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-
         title: Text(widget.title),
       ),
-      body: Center(
-
-        child: Column(
-
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Row(
+          children: [
+            // Colonne de gauche
+            const Expanded(
+              flex: 2,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Pavlova aux fraises',
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(height: 8.0),
+                  Text(
+                    'La Pavlova est un dessert à base de meringue qui doit son nom à la ballerine russe Anna Pavlova. '
+                    'La Pavlova se caractérise par une croûte croustillante et un intérieur moelleux et léger, garni '
+                    'de fruits et de crème fouettée.',
+                  ),
+                  SizedBox(height: 16.0),
+                  Text('★★★★★  170 Notes', style: TextStyle(fontSize: 16)),
+                  SizedBox(height: 16.0),
+                  Text('Préparation : 25 min'),
+                  Text('Cuisson : 1 hr'),
+                  Text('Nb. Pers. : 4-6'),
+                ],
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            const SizedBox(width: 16.0),
+
+            Expanded(
+              flex: 1,
+              child: Center(
+                child: Image.network(
+                  'https://www.lavieclaire.com/wp-content/uploads/2023/04/site_Pavlova-aux-fraises-1-scaled.jpg',
+                  fit: BoxFit.cover,
+                ),
+              ),
             ),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ),
     );
   }
